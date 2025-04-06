4941efc7597005f8b5afd1138c514d9c73f81c68
ProjectBelus <belusproject@gmail.com>
2025-04-06 16:29:35 -0500

    Fix root route to land on Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to top on mount
+  }, []);
   return (
     <main>
       <Hero />
